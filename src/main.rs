844545eab6cb192fa26e855b30531a00ca867305
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-19 18:17:04 +0100

    Server index.html

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
@@ -15,14 +16,17 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("{:#?}", http_request);
+    // println!("{:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let length = contents.len();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
