ba6cf115734b4f95ca9ea989b07811693ef87b1a
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-22 23:20:42 +0100

    Sleep endpoint for testing slow response time

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use std::{
     fs,
     io::{prelude::BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
-    let listener = TcpListener::bind("localhost:6969").unwrap();
+    let listener = TcpListener::bind("localhost:42069").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -18,11 +20,15 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
+
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
